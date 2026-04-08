3dc5dbcd4bec94e9d68c42bb80d507fa971bce4e
MuziGaeunOwner <dlwnals94@gmail.com>
2026-04-09 00:43:00 +0900

    react-grid-layout 버전 에러 수정

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Responsive, WidthProvider } from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout/legacy';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import { 
